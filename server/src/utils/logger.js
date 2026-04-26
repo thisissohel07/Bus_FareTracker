50daa34cfb0f47e13862e9de7da69b42117d61dc
Shaik Sohel <thisissohel07@gmail.com>
2026-04-27 02:30:03 +0530

    Fix Winston logging directory crash in Docker

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -7,6 +7,12 @@
 
 const winston = require('winston');
 const path = require('path');
+const fs = require('fs');
+
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 
 const logger = winston.createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
